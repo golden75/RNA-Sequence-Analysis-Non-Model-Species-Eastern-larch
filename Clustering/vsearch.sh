86d8329c1bb45a817f39f7e24d9fea60e92bd7fd
Your Name <you@example.com>
2019-05-06 10:31:12 -0400

    change path

--- Clustering/vsearch.sh
+++ Clustering/vsearch.sh
@@ -2,48 +2,28 @@
 #SBATCH --job-name=vsearch
 #SBATCH -n 1
 #SBATCH -N 1
-#SBATCH -c 36
-#SBATCH --mem=120G
+#SBATCH -c 48
+#SBATCH --mem=200G
 #SBATCH --partition=general
 #SBATCH --qos=general
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=neranjan.perera@uconn.edu
+#SBATCH --mail-user=first.last@uconn.edu
 #SBATCH -o %x_%j.out
 #SBATCH -e %x_%j.err
 
 hostname
 date
 
-# Combind the assembled fasta files
 
-module load trinity/2.6.6
+# Clustering using vsearch
+module load vsearch/2.4.3
 
-Trinity --seqType fq \
-	--left ../Quality_Control/trim_U13_R1.fastq \
-	--right ../Quality_Control/trim_U13_R2.fastq \
-	--min_contig_length 300 \
-	--CPU 36 \
-	--max_memory 120G \
-	--output trinity_U13 \
-	--full_cleanup 
+vsearch --threads 32 --log LOGFile \
+	--cluster_fast ../Coding_Regions/trinity_combine.fasta.transdecoder.cds \
+	--id 0.90 \
+	--centroids centroids.fasta \
+	--uc clusters.uc
 
-Trinity --seqType fq \
-	--left ../Quality_Control/trim_U32_R1.fastq \
-	--right ../Quality_Control/trim_U32_R2.fastq \
-	--min_contig_length 300 \
-	--CPU 36 \
-	--max_memory 120G \
-	--output trinity_U32 \
-	--full_cleanup  
-
-Trinity --seqType fq \
-	--left ../Quality_Control/trim_K32_R1.fastq \
-	--right ../Quality_Control/trim_K32_R2.fastq \
-	--min_contig_length 300 \
-	--CPU 36 \
-	--max_memory 120G \
-	--output trinity_K32 \
-	--full_cleanup 
 
 date 
 
