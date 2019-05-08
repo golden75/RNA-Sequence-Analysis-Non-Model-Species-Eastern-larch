2323ab66806f4cdbd376249e8f5a4008c358d022
Your Name <you@example.com>
2019-05-08 01:03:46 -0400

    add gfold.sh

---
+++ Gfold/gfold.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=gfold
+#SBATCH -n 1
+#SBATCH -N 1
+#SBATCH -c 1
+#SBATCH --mem=30G
+#SBATCH --partition=general
+#SBATCH --qos=general
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=neranjan.perera@uconn.edu
+#SBATCH -o %x_%j.out
+#SBATCH -e %x_%j.err
+
+hostname
+date
+##########################################
+## Gfold				## 
+##########################################
+module load gfold/1.1.4 
+
+gfold diff -s1 K32 -s2 K23 -suf .read_cnt -o K32_vs_K23.diff
+
+date 
+
+
