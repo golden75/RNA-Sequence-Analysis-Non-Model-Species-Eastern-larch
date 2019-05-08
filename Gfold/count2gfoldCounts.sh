66cf8c720ee8dbea4c34ac0a21bfd98d66302b2b
Your Name <you@example.com>
2019-05-08 18:02:11 -0400

    remove U libraries

--- Gfold/count2gfoldCounts.sh
+++ Gfold/count2gfoldCounts.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-awk '{print $1 "\t" $1 "\t" $4 "\t" $2 "\t" $5 }' ../Counts/K23/abundance.tsv > K23_cnt
-awk '{print $1 "\t" $1 "\t" $4 "\t" $2 "\t" $5 }' ../Counts/K32/abundance.tsv > K32_cnt
-awk '{print $1 "\t" $1 "\t" $4 "\t" $2 "\t" $5 }' ../Counts/U13/abundance.tsv > U13_cnt
-awk '{print $1 "\t" $1 "\t" $4 "\t" $2 "\t" $5 }' ../Counts/U32/abundance.tsv > U32_cnt
+awk '{print $1 "\t" $1 "\t" $4 "\t" $2 "\t" $5 }' ../Counts/K23/abundance.tsv > K23.read_cnt
+awk '{print $1 "\t" $1 "\t" $4 "\t" $2 "\t" $5 }' ../Counts/K32/abundance.tsv > K32.read_cnt
 
